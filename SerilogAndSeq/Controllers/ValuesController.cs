232dab4f661bb723bacd90c8362a03999c23cf17
tanersaydam <tanersaydam@gmail.com>
2023-10-28 00:54:14 +0300

    Add project files.

---
+++ SerilogAndSeq/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SerilogAndSeq.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{    
+    [HttpGet]
+    public IActionResult Get(int number)
+    {
+        int x = 0;
+        try
+        {
+            int z = x / number;
+            return Ok(new { Message = $"Result is {z}"});
+        }
+        catch (Exception ex)
+        {            
+            throw new Exception(ex.Message);
+        }          
+    }
+}
---
+++ SerilogAndSeq/Program.cs
@@ -0,0 +1,30 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLogging();
+
+builder.Host.UseSerilog((context, loggerConfig) => 
+loggerConfig.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseSerilogRequestLogging();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
